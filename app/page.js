b5da72cfc730fe45a5a58f3978c1483e529541b1
Usman Khalid <usmanchaudrhy@gmail.com>
2024-02-16 00:13:19 +1100

    init next project

---
+++ app/page.js
@@ -0,0 +1,30 @@
+"use client"
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+  
+
+export default function Home() {
+  return (
+    <main>
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>            
+            </NavigationMenuList>
+        </NavigationMenu>  
+    </main>
+  );
+}
